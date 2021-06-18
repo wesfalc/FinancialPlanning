a0d5261fa1f431a433716640516b3959e448fe0e
Wesley Falcao <wesfalc@outlook.com>
2021-06-18 15:46:00 -0400

    Initial commit.

---
+++ src/main/java/com/wesfalc/FinancialPlanningApplication.java
@@ -0,0 +1,13 @@
+package com.wesfalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialPlanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialPlanningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wesfalc/FinancialPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesfalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialPlanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
